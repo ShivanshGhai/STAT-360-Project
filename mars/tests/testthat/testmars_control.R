082f6bde7d1cc7ac5bf686fe9db8d03863298e8d
Shivansh Ghai <shivanshghai@Shivanshs-MacBook-Pro-8.local>
2026-03-29 23:34:26 -0700

    Add project test data files

--- mars/tests/testthat/testmars_control.R
+++
@@ -1,7 +0,0 @@
-test_that('mars.control() returns a valid control object', {
-  mc <- mars.control(Mmax = 10)
-  expect_s3_class(mc, 'mars.control')
-  expect_equal(mc$Mmax, 10L)
-  expect_equal(mc$d, 3)
-  expect_equal(mc$trace, FALSE)
-})
